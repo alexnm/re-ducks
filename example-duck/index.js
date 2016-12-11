fb5ef3538267805c4dced1b3f225bdee6dc5f233
alexnm <alex.n.moldovan@gmail.com>
2016-12-11 12:59:48 +0200

    update

--- example-duck/index.js
+++ example-duck/index.js
@@ -1,6 +1,9 @@
 /* INDEX FILE
 
-This file behaves as the duck file form the original proposal
+This file, from a module perspective, behaves as the duck file form the original proposal.
+It exports as default the reducer function of the duck.
+It exports as named export the selectors and the operations.
+Optionally it exports the actions and types if they are needed in other ducks.
 
 */
 
